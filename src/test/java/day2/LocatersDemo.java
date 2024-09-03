5140b56815609b2c031fdaa19060cf372339105f
kanhai kumar <kanhaikumar961@gmail.com>
2024-09-03 19:16:18 +0530

    third commit

---
+++ src/test/java/day2/LocatersDemo.java
@@ -0,0 +1,31 @@
+package day2;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+public class LocatersDemo {
+
+	public static void main(String[] args) {
+     WebDriver driver=new ChromeDriver();
+     driver.get("https://demo.opencart.com/");
+     driver.manage().window().maximize();
+     
+     //name
+    //driver.findElement(By.name("search")).sendKeys("Mac");
+     //id
+    //boolean logoDisplaystatus=driver.findElement(By.id("logo")).isDisplayed();
+    //System.out.println(logoDisplaystatus);
+     
+     //link test & partial link text
+    // driver.findElement(By.linkText("Tablets")).click();
+    // driver.findElement(By.partialLinkText("ets")).click();
+    //FIND SIMIAR HOW MANY LIKS ARE THERE IN HEADER?
+    List<WebElement> headerLinks=driver.findElements(By.className("list-inline-item"));
+    System.out.print("total number of header links:"+headerLinks.size());
+     
+	}
+
+}
